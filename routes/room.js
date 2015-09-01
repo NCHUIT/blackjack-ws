11f701cd6b3c6768173f449f449c4ef0dcc911a9
jd615645 <jd615645@gmail.com>
2015-09-01 22:53:43 +0800

    :cat2:前端配置進行中...

--- routes/room.js
+++ routes/room.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res ,next) {
-    res.render('index', { title: 'Observer'});
+    res.render('observer/index', { title: 'Observer'});
 });
 
 module.exports = router;
