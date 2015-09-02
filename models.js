45b51f9dcb963626e67ad27dcff732f8765c317f
taichunmin <taichunmin@gmail.com>
2015-09-02 21:14:11 +0800

    Room model 初步完成

--- models.js
+++ models.js
@@ -6,7 +6,6 @@ function Room() {
   this.VALUES = {a:1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "t":10, "j":10, "q":10, "k":10};
 
   this.init();
-  this.start();
 }
 
 Room.prototype.init = function(){
@@ -18,11 +17,13 @@ Room.prototype.init = function(){
 };
 
 Room.prototype.start = function() {
-  if(this.players.length < 1)
+  this.deck = new Deck(this).shuffle();
+  this.in_play = true;
+  if(this.players.length < 2)
     throw "[Room.start] Players count must between 2 and 4.";
   for(i=0; i<2; i++)
-    for(player in this.players)
-      player.hand.addCard( this.deck.dealCard() );
+    for(j in this.players)
+      this.players[j].addCard( this.deck.dealCard() );
   this.outcome("<strong>Hit</strong> or <strong>stand</strong>?");
   // if(in_play) {
   //   score -= 1;
@@ -49,27 +50,27 @@ Room.prototype.addPlayer = function(socketId) {
     throw "[Room.addPlayer] exceed 4 players";
   var player = new Person(socketId);
   player.room = this;
-  this.players.push(player);
+  this.players[socketId] = player;
   return player;
 }
 
 Room.prototype.addObserver = function(socketId) {
   var observer = new Person(socketId);
   observer.room = this;
-  this.observers.push(observer);
+  this.observers[socketId] = observer;
   return observer;
 }
 
 Room.prototype.draw = function() {
-  for(player in this.players)
-    player.drawCards().drawValue();
-  for(observer in this.observers)
-    observer.draw();
+  for(i in this.players)
+    this.players[i].drawCards().drawValue();
+  for(i in this.observers)
+    this.observers[i].drawObserver();
 }
 
 // Card
-function Card(suit, rank) {
-  if( SUITS.indexOf(suit) == -1 || RANKS.indexOf(rank) == -1 )
+function Card(room, suit, rank) {
+  if( room.SUITS.indexOf(suit) == -1 || room.RANKS.indexOf(rank) == -1 )
     throw "[Card.construct] Invalid card: " + suit + rank;
   this.suit = suit;
   this.rank = rank;
@@ -87,11 +88,13 @@ Card.prototype = {
 };
 Room.prototype.Card = Card;
 
-function Deck() {
+function Deck(room) {
+  this.room = room;
   this.cards = [];
+  var SUITS = this.room.SUITS, RANKS = this.room.RANKS;
   for( var i in SUITS )
     for( var j in RANKS )
-      this.cards.push(new Card(SUITS[i], RANKS[j]));
+      this.cards.push(new Card(room, SUITS[i], RANKS[j]));
   return this;
 };
 Deck.prototype = {
@@ -114,7 +117,6 @@ Room.prototype.Deck = Deck;
 
 function Person(socketId) {
   this.nick = socketId;
-  this.hand = null;
   this.room = null;
   this.cards = [];
   this.socketId = socketId;
@@ -123,12 +125,12 @@ Person.prototype = {
   hit: function() {
     if(this.room.in_play) {
       if(this.getValue() <= 21)
-        this.add_card(this.room.deck.deal_card());
+        this.addCard(this.room.deck.dealCard());
       if(this.getValue() > 21)
         this.room.outcome("<strong>" + this.nick + "</strong> have busted", "warning");
       this.room.draw();
     } else {
-      this.outcome("Click <strong>Deal</strong> button to restart.");
+      this.outcome("Click <strong>Restart</strong> button to restart.");
     }
   },
   stand: function() {
@@ -138,7 +140,7 @@ Person.prototype = {
     // }
     // if(player.getValue() <= 21)
     //   while(dealer.getValue() < 17 || dealer.getValue() < Math.min(player.getValue(), 21))
-    //     dealer.add_card(deck.deal_card());
+    //     dealer.addCard(deck.dealCard());
     // in_play = false;
     // player_win = ( player.getValue() <= 21 && dealer.getValue() < player.getValue() || dealer.getValue() > 21 );
     // if(player_win) {
@@ -160,16 +162,16 @@ Person.prototype = {
     this.cards.push(card);
   },
   getValue: function(steal) {
-    var value = 0, hasAce = false, isFirst = (steal === true);
+    var value = 0, hasAce = false, isFirst = (steal === true), VALUES = this.room.VALUES;
     for( var i in this.cards ) {
       if(isFirst) {
         isFirst = false;
         continue;
       }
       card = this.cards[i];
-      if( card.get_rank() == 'a' )
+      if( card.getRank() == 'a' )
         hasAce = true;
-      value += VALUES[card.get_rank()];
+      value += VALUES[card.getRank()];
     }
     if(hasAce && value<=11)
       value += 10;
@@ -177,8 +179,11 @@ Person.prototype = {
       return 21;
     return value;
   },
+  drawObserver: function() {
+
+  },
   drawValue: function(steal) {
-    console.log("[Person.drawValue] " + this.getValue());
+    console.log("[Person.drawValue] " + this.getValue(steal));
     return this;
   },
   drawCards: function(steal) {
@@ -187,10 +192,15 @@ Person.prototype = {
       cards[0] = 'xx';
     // for(var i in this.cards) {
     // }
-    console.log(cards.join(" "));
+    console.log('[Person.drawCards] ' + cards.join(" "));
     return this;
   },
 };
 Room.prototype.Person = Person;
 
 // module.exports = Room;
+
+r = new Room();
+p1 = r.addPlayer('1');
+p2 = r.addPlayer('2');
+r.start();
